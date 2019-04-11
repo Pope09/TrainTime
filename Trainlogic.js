b27c6a5cc6205e0ab0e6001342a3d4a5c28b0403
Pope09 <narrowways@yahoo.com>
2019-04-10 20:21:32 -0600

    final update

--- Trainlogic.js
+++ Trainlogic.js
@@ -21,13 +21,20 @@ var NextArrival = "";
 var MinutesAway = 0;
 console.log('javascript is working');
 $("#submit").on("click", function (event) {
-
     event.preventDefault();
-
+    
     TrainName = $("#TrainName").val().trim();
     TrainDestination = $("#destination").val().trim();
     Frequency = $("#frequency").val().trim();
     TrainTime = $("#time").val().trim();
+    
+    console.log(TrainName);
+    console.log(TrainDestination);
+    console.log(TrainTime);
+    console.log(Frequency);
+    
+
+    
 
     if (TrainName === "" || TrainDestination === "" || Frequency === "" || TrainTime === "") {
         alert("Please fill out the form");
@@ -51,6 +58,35 @@ $("#submit").on("click", function (event) {
     $("#time").val();
 });
 
+function nextTrain (startime,tFrequency){
+    console.log(startime,tFrequency);
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(startime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train  
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    return [tMinutesTillTrain, nextTrain];
+}
+
 database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
 
     // Log everything that's coming out of snapshot
@@ -59,6 +95,7 @@ database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     console.log(snapshot.val().TrainDestination);
     console.log(snapshot.val().Frequency);
     console.log(snapshot.val().TrainTime);
+    var time = nextTrain(snapshot.val().TrainTime, snapshot.val().Frequency );
 
     
 
@@ -66,12 +103,12 @@ database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     var row = $("<tr>");
     var tdName = $("<td>").text(snapshot.val().TrainName);
     var tdDestination = $("<td>").text(snapshot.val().TrainDestination);
-    var tdFrequency = $("<td>").text(Frequency);
-    var tdNextArival = $("<td>").text(snapshot.val().NextArrival);
-    var tdMinsAway = $("<td>").text(snapshot.val().MinutesAway);
+    var tdFrequency = $("<td>").text(snapshot.val().Frequency);
+    var tdNextArival = $("<td>").text(time[1].format("hh:mm"));
+    var tdMinsAway = $("<td>").text(time[0]);
 
     row.append(tdName, tdDestination, tdFrequency, tdNextArival, tdMinsAway);
-    $("#tbody").append(row);
+    $("tbody").append(row);
 
     // Handle the errors
 }, function (errorObject) {
